40554339bbb5bf56dfd5f802fdf3f0e20ecfcd15
Julian Allende <julianallenderussek@gmail.com>
2021-10-26 11:34:22 -0500

    article edti and update done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,39 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
+    end
 
+    def index 
+        @articles = Article.all
+    end
+
+    def new 
+       @article = Article.new
+    end
+
+    def create 
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created succesfully."
+        #Then we show which page we redirect to | Ruby will stract the correspondent article id from the article variable
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated succesfully"
+            redirect_to @article
+        else 
+            render 'edit '
+        end
     end
 
 end
