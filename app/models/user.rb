b8951b6c3143c2c7ec6475f5b29d8508a97d6818
Julian Allende <julianallenderussek@gmail.com>
2021-10-27 00:40:14 -0500

    user validations done

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    validates :username, 
+               presence: true, 
+               length: {minimum: 3, maximum: 25}, 
+               uniqueness: { case_sensitive: false }
+    VALID_EMAIL_REGEX = /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, 
+                presence: true, 
+                uniqueness: { case_sensitive: false },
+                format: { with: VALID_EMAIL_REGEX}
+end
